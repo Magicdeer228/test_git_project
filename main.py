d7621e9e62b243eac235242c63d8d8992d421992
Magicdeer228 <94570337+Magicdeer228@users.noreply.github.com>
2022-03-22 03:29:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,134 @@
-from flask import Flask, url_for, render_template
+# импортируем библиотеки
+from flask import Flask, request
+import logging
 
+# библиотека, которая нам понадобится для работы с JSON
+import json
+
+# создаём приложение
+# мы передаём __name__, в нем содержится информация,
+# в каком модуле мы находимся.
+# В данном случае там содержится '__main__',
+# так как мы обращаемся к переменной из запущенного модуля.
+# если бы такое обращение, например,
+# произошло внутри модуля logging, то мы бы получили 'logging'
 app = Flask(__name__)
 
+# Устанавливаем уровень логирования
+logging.basicConfig(level=logging.INFO)
 
-@app.route('/')
-def start():
-    return render_template('base.html')
-
-
-@app.route('/index')
-def index():
-    return render_template('index.html', text=['И на марсе будут яблони цвести!'])
-
-
-@app.route('/promotion')
-def promotion():
-    with open('db/promotion.txt', 'r', encoding='UTF-8') as text:
-        return render_template('index.html', text=text.readlines())
-
-
-@app.route('/image_mars')
-def image_mars():
-    image_url = url_for('static', filename='img/img.jpg')
-    return render_template('for_picture.html', img_url=image_url)
+# Создадим словарь, чтобы для каждой сессии общения
+# с навыком хранились подсказки, которые видел пользователь.
+# Это поможет нам немного разнообразить подсказки ответов
+# (buttons в JSON ответа).
+# Когда новый пользователь напишет нашему навыку,
+# то мы сохраним в этот словарь запись формата
+# sessionStorage[user_id] = {'suggests': ["Не хочу.", "Не буду.", "Отстань!" ]}
+# Такая запись говорит, что мы показали пользователю эти три подсказки.
+# Когда он откажется купить слона,
+# то мы уберем одну подсказку. Как будто что-то меняется :)
+sessionStorage = {}
 
 
-@app.route('/promotion_image')
-def promotion_image():
-    with open('db/promotion.txt', 'r', encoding='UTF-8') as text:
-        image_url = url_for('static', filename='img/img.jpg')
-        style = url_for('static', filename='css/style.css')
-        return render_template('promotion_image.html', text=text.readlines(), img_url=image_url, style=style)
+@app.route('/post', methods=['POST'])
+# Функция получает тело запроса и возвращает ответ.
+# Внутри функции доступен request.json - это JSON,
+# который отправила нам Алиса в запросе POST
+def main():
+    logging.info(f'Request: {request.json!r}')
+
+    # Начинаем формировать ответ, согласно документации
+    # мы собираем словарь, который потом при помощи
+    # библиотеки json преобразуем в JSON и отдадим Алисе
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    # Отправляем request.json и response в функцию handle_dialog.
+    # Она сформирует оставшиеся поля JSON, которые отвечают
+    # непосредственно за ведение диалога
+    handle_dialog(request.json, response)
+
+    logging.info(f'Response:  {response!r}')
+
+    # Преобразовываем в JSON и возвращаем
+    return json.dumps(response)
 
 
-@app.route('/astronaut_selection')
-def astronaut_selection():
-    style = url_for('static', filename='css/style.css')
-    return render_template('astronaut_selection_form.html', style=style)
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+        # Запишем подсказки, которые мы ему покажем в первый раз
+
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        # Заполняем текст ответа
+        res['response']['text'] = 'Привет! Купи слона!'
+        # Получим подсказки
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+
+    # Сюда дойдем только, если пользователь не новый,
+    # и разговор с Алисой уже был начат
+    # Обрабатываем ответ пользователя.
+    # В req['request']['original_utterance'] лежит весь текст,
+    # что нам прислал пользователь
+    # Если он написал 'ладно', 'куплю', 'покупаю', 'хорошо',
+    # то мы считаем, что пользователь согласился.
+    # Подумайте, всё ли в этом фрагменте написано "красиво"?
+    if req['request']['original_utterance'].lower() in [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо'
+    ]:
+        # Пользователь согласился, прощаемся.
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
+
+    # Если нет, то убеждаем его купить слона!
+    res['response']['text'] = \
+        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
+    res['response']['buttons'] = get_suggests(user_id)
 
 
-@app.route('/choice/<planet_name>')
-def choice(planet_name):
-    planets_text = {
-        'Марс': ['Эта планета близка к Земле;', 'На ней много необходимых ресурсов;', 'На ней есть вода и атмосфера;',
-                 'На ней есть небольшое магнитное поле;', 'Наконец, она просто красива!'],
-        'Меркурий': ['Он ближайшая к Солнцу планета;', 'Она движется по небу быстрее других планет;',
-                     'Он относится к планетам земной группы;', 'Летим на Меркурий?']}
+# Функция возвращает две подсказки для ответа.
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
 
-    if planet_name in planets_text:
-        text = planets_text[planet_name]
-    else:
-        text = []
-    return render_template('choice_planet.html', planet=planet_name, text=text)
+    # Выбираем две первые подсказки из массива.
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
 
+    # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
 
-@app.route('/results/<nickname>/<int:level>/<float:rating>')
-def results(nickname, level, rating):
-    return render_template('results.html', nickname=nickname, level=level, rating=rating)
+    # Если осталась только одна подсказка, предлагаем подсказку
+    # со ссылкой на Яндекс.Маркет.
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
 
-
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    return suggests
 
 
 
